4c7c77b5f2e7d2e7c3be6249541df4bc25b47024
green4x <red4x.mail@gmail.com>
2018-08-10 16:02:55 +0300

    -

--- vesna/urls.py
+++ vesna/urls.py
@@ -14,8 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls import include
 from django.urls import path
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('electricBills/', include('electricBills.urls')),
+    path('', RedirectView.as_view(url='/electricBills/')),
 ]
+
+# Use static() to add url mapping to serve static files during development (only)
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
